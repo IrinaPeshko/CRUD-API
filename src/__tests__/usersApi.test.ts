8a91a13bab2ed1a257d12ad20f85a716077a515e
IrinaPeshko <privet-kira1999@mail.ru>
2024-02-12 23:42:11 +0300

    fix: change tests

--- src/__tests__/usersApi.test.ts
+++ src/__tests__/usersApi.test.ts
@@ -3,7 +3,7 @@ import server from '../index';
 
 const request = supertest(server);
 
-describe('GET /api/users', () => {
+describe('test endpoints', () => {
   let createdUserId = '';
 
   const newUser = {
@@ -11,10 +11,6 @@ describe('GET /api/users', () => {
     age: 24,
     hobbies: ['Reading', 'Traveling'],
   };
-
-  afterAll((done) => {
-    server.close(done);
-  });
 
   it('return all users', async () => {
     const response = await request.get('/api/users');
@@ -70,6 +66,79 @@ describe('GET /api/users', () => {
     expect(newResponse.statusCode).toBe(404);
     expect(newResponse.body.message).toBe(
       `The user with id: '${createdUserId}' not found`,
+    );
+  });
+});
+
+describe('valid data', () => {
+  it('returns 400 for invalid age', async () => {
+    const invalidUser = {
+      username: 'New user',
+      age: 'invalid_age',
+      hobbies: ['Reading', 'Traveling'],
+    };
+
+    const response = await request.post('/api/users').send(invalidUser);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe('Invalid age');
+  });
+
+  it('returns 400 for invalid name', async () => {
+    const incompleteUser = {
+      age: 24,
+      hobbies: ['Reading', 'Traveling'],
+    };
+
+    const response = await request.post('/api/users').send(incompleteUser);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe('Invalid username');
+  });
+
+  it('returns 400 for invalid hobbies', async () => {
+    const incompleteUser = {
+      name: 'Irina',
+      age: 24,
+      hobbies: 'Reading',
+    };
+
+    const response = await request.post('/api/users').send(incompleteUser);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe('Invalid fields in user data');
+  });
+});
+
+describe('error handling', () => {
+  const userId = '94156b01-0258-4276-b542-5cc5bd527c8d';
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  it('returns 404 when trying to get a non-existent user', async () => {
+    const response = await request.get(`/api/users/${userId}`);
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(
+      `The user with id: '${userId}' not found`,
+    );
+  });
+
+  it('returns 400 when trying to update a user with invalid data', async () => {
+    const invalidUpdateData = {
+      username: null,
+      age: -5,
+      hobbies: ['Reading', 123],
+    };
+    const response = await request
+      .put(`/api/users/${userId}`)
+      .send(invalidUpdateData);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBeDefined();
+  });
+
+  it('returns 400 when trying to delete a user with invalid UUID', async () => {
+    const response = await request.delete(`/api/users/${userId}`);
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(
+      "The user with id: '94156b01-0258-4276-b542-5cc5bd527c8d' not found",
     );
   });
 });
